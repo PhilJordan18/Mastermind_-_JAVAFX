264e23cc0cf532a0d3db028349dfb3230f52447c
PhilJordan18 <philippemonf@yahoo.com>
2024-04-11 17:10:06 -0400

    Initializing the application

---
+++ src/main/java/com/example/javafx__mastermind/Game.java
@@ -0,0 +1,4 @@
+package com.example.javafx__mastermind;
+
+public class Game {
+}
---
+++ src/main/java/com/example/javafx__mastermind/HelloApplication.java
@@ -0,0 +1,35 @@
+package com.example.javafx__mastermind;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class HelloApplication extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        stage.setTitle("Hello!");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+
+    @Override
+    public void init() throws Exception {
+        super.init();
+        System.out.println("INIT");
+    }
+
+    @Override
+    public void stop() throws Exception {
+        super.stop();
+        System.out.println("REVERSAL CURSE TECHNIQUE : RED !!!");
+    }
+}
